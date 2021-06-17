1e5c74e4200af838c80648ce7f58625b4818d689
Joao Silva <josejoaosilva.n@gmail.com>
2021-06-17 11:55:10 -0300

    first commit

---
+++ carvedingYou/carvedingYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  carvedingYouApp.swift
+//  carvedingYou
+//
+//  Created by José João Silva Nunes Alves on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct carvedingYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
