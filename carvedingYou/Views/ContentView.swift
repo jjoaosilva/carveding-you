05f04389d4d4944860b729840edda5350ce8ad66
Joao Silva <josejoaosilva.n@gmail.com>
2021-06-21 09:25:23 -0300

    using camera

--- carvedingYou/Views/ContentView.swift
+++ carvedingYou/Views/ContentView.swift
@@ -10,9 +10,44 @@ import SwiftUI
 struct ContentView: View {
     let viewModel: CarvingViewModel = CarvingViewModel()
 
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+
     var body: some View {
-        Text("Carveding you")
-            .padding()
+        NavigationView {
+            VStack {
+
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                } else {
+                    Image(systemName: "snow")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                }
+
+                Button("Camera") {
+                    self.sourceType = .camera
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+
+                Button("photo") {
+                    self.sourceType = .photoLibrary
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+            }
+            .navigationBarTitle("Demo")
+            .sheet(isPresented: self.$isImagePickerDisplay) {
+                ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+            }
+
+        }
     }
 }
 
